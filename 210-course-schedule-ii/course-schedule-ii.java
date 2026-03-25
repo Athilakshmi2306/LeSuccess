266122cee6fe0d2e2f204a3432edf8639ac2c57e
Athilakshmi2306 <athilakshmi2306@gmail.com>
2026-03-25 11:48:13 +0530

    Time: 7 ms (35.31%) | Memory: 47.8 MB (25.10%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] findOrder(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge:edges){
+            adj.get(edge[1]).add(edge[0]);
+        }
+        int[] ind=new int[V];
+        for(int i=0;i<V;i++){
+            for(int n:adj.get(i)){
+                ind[n]++;
+            }
+        }
+        
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(ind[i]==0){
+                q.add(i);
+            }
+        }
+        int[] res=new int[V];
+        int index=0;
+        while(!q.isEmpty()){
+            int vertex=q.poll();
+            res[index++]=vertex;
+            for(int nei:adj.get(vertex)){
+                ind[nei]--;
+                if(ind[nei]==0){
+                    q.add(nei);
+                }
+            }
+        }
+        return index==V? res:new int[0];
+    }
+}
