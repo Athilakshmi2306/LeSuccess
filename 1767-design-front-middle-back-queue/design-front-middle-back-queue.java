3ffaf980fb23f6826d583fb9efe9500a8fa0203d
Athilakshmi2306 <athilakshmi2306@gmail.com>
2026-08-19 13:55:36 +0530

    Time: 9 ms (35.61%) | Memory: 46.8 MB (95.17%) - LeetSync

---
+++ 1767-design-front-middle-back-queue/design-front-middle-back-queue.java
@@ -0,0 +1,51 @@
+class FrontMiddleBackQueue {
+ LinkedList<Integer> list;
+    public FrontMiddleBackQueue() {
+        list =new LinkedList<>();
+    }
+    
+    public void pushFront(int val) {
+        list.addFirst(val);
+    }
+    
+    public void pushMiddle(int val) {
+        int mid=list.size()/2;
+        list.add(mid,val);
+    }
+    
+    public void pushBack(int val) {
+        list.addLast(val);
+    }
+    
+    public int popFront() {
+         if (list.isEmpty())
+            return -1;
+
+        return list.removeFirst();
+    }
+    
+    public int popMiddle() {
+        if(list.isEmpty())
+        return -1;
+        int mid=(list.size()-1)/2;
+        return list.remove(mid);
+    }
+    
+    public int popBack() {
+        if (list.isEmpty())
+            return -1;
+
+        return list.removeLast();
+    }
+}
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue obj = new FrontMiddleBackQueue();
+ * obj.pushFront(val);
+ * obj.pushMiddle(val);
+ * obj.pushBack(val);
+ * int param_4 = obj.popFront();
+ * int param_5 = obj.popMiddle();
+ * int param_6 = obj.popBack();
+ */
