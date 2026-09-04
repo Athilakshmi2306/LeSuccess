4962802c79e1297bcc0e5d1e60c39411f996b83f
Athilakshmi2306 <athilakshmi2306@gmail.com>
2026-09-04 11:48:39 +0530

    Time: 31 ms (56.28%) | Memory: 48.3 MB (84.59%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < candyType.length; i++) {
+            set.add(candyType[i]);
+        }
+
+        return Math.min(set.size(), candyType.length / 2);
+    }
+}
