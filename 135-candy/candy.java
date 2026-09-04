f52f93ab011d9cdacc3713b2756eb93efb1f1fbb
Athilakshmi2306 <athilakshmi2306@gmail.com>
2026-09-04 11:28:36 +0530

    Time: 4 ms (18.88%) | Memory: 52.3 MB (15.70%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] a=new int[n];
+        int[] b=new int[n];
+        Arrays.fill(a,1);
+        Arrays.fill(b,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                a[i]=a[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                b[i]=b[i+1]+1;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=Math.max(a[i],b[i]);
+        }
+        return sum;
+    }
+}
